d4d6cd6521641dada829e627a593c960cbdb4ccb
Emma <ebourgeois2021@k12.andoverma.us>
2021-03-23 10:46:20 -0400

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,20 +1,8 @@
-
-// Used to toggle the menu on small screens when clicking on the menu button
-function toggleFunction() {
-  var x = document.getElementById("smNav");
-  if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-  } else {
-      x.className = x.className.replace(" w3-show", "");
-  }
-} 
-
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += "responsive";
-  } else {
-    x.className = "topnav";
-  }
+function mobileDropNav() {
+    var x = document.getElementById("dropNav");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
 }
